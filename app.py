51494493331395176e1f5d6774e365a0991e0d7b
Ubuntu <ubuntu@ip-10-101-1-61.eu-central-1.compute.internal>
2021-04-21 15:20:14 +0200

    endpoints are in a first working state

--- app.py
+++ app.py
@@ -1,19 +1,163 @@
 import os
-from flask import Flask, request, abort, jsonify
+import sys
+from flask import Flask, request, abort, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-from models import setup_db
+from models import setup_db, Developer, Sprint, Vacation
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  CORS(app)
+  CORS(app, resources={r"/api/*": {'origins': '*'}})
+  app.secret_key = os.getenv('SECRET_KEY')
 
-  @app.route('/')
+  @app.route('/', methods=['GET'])
   def index():
-    return 'Hello world!'
+    return '<h1> Welcome to the Sprint Velocity Planning Tool SprintVel! </h1>', 200
+
+  @app.route('/developers', methods=['GET'])
+  def get_developers():
+    developers = Developer.query.all()
+    return jsonify({
+      'success': True,
+      'developers': developers
+    }), 200
+
+  @app.route('/sprints', methods=['GET'])
+  def get_sprints():
+    sprints = Sprint.query.all()
+    return jsonify({
+      'success': True,
+      'sprints': sprints
+    }), 200
+
+  @app.route('/developers', methods=['POST'])
+  def post_developers():
+
+    error = False
+    body = request.get_json()
+    #print(body)
+    name = body.get('name')
+    #print(name)
+    proj_participation = body.get('proj_participation')
+    #print(proj_participation)
+
+    new_developer = Developer(name=name, proj_participation=proj_participation)
+
+    try:
+      new_developer.insert()
+      flash('New Developer ' + str(new_developer.name) + ' was successful listed!')
+    except Exception as e:
+      error = True
+      print(e)
+      flash('An error occurred. New Developer ' + str(new_developer.name) + ' could not be listed.')
+    if error:
+      abort(400)
+    else:
+      return jsonify({
+        'success': True,
+        'name': name,
+        'proj_participation': proj_participation
+      }), 200
+
+  @app.route('/sprints', methods=['POST'])
+  def post_sprints():
+
+    error = False
+    body = request.get_json()
+    #print(body)
+    name = body.get('name')
+    sp_planned = body.get('sp_planned')
+    sp_finished = body.get('sp_finished')
+    velocity_factor = body.get('velocity_factor')
+    sp_predicitions_next_sprint = body.get('sp_prediction_next_sprint')
+    sprint_fte_sum = body.get('sprint_fte_sum')
+    date_start = body.get('date_start')
+    date_end = body.get('date_end')
+
+
+    new_sprint = Sprint(name=name, sp_planned=sp_planned, sp_finished=sp_finished, 
+    velocity_factor=velocity_factor, sp_predicitions_next_sprint=sp_predicitions_next_sprint,
+    sprint_fte_sum=sprint_fte_sum, date_start=date_start, date_end=date_end)
+
+    try:
+      new_sprint.insert()
+      flash('New Sprint ' + str(new_sprint.name) + ' was successful listed!')
+    except Exception as e:
+      error = True
+      print(e)
+      flash('An error occurred. New Sprint ' + str(new_sprint.name) + ' could not be listed.')
+    if error:
+      abort(400)
+    else:
+      return jsonify({
+        'success': True,
+        'name': name,
+        'sp_planned': sp_planned,
+        'sp_finished': sp_finished,
+        'velocity_factor': velocity_factor,
+        'sp_prediction_next_sprint': sp_predicitions_next_sprint,
+        'sprint_fte_sum': sprint_fte_sum,
+        'date_start': date_start,
+        'date_end': date_end
+      }), 200
+
+  @app.route('/developers/<int:developer_id>', methods=['PATCH'])
+  def patch_an_existing_developer(developer_id):
+    body = request.get_json()
+    name = body.get('name')
+    proj_participation = body.get('proj_participation')
+
+    try:
+        developer = Developer.query.get(developer_id)
+    except Exception as e:
+        print(e)
+        abort(404)
+
+    try:
+        if name == None:
+            None
+        else:
+            developer.name = name
+        if proj_participation == None:
+            None
+        else:
+            developer.proj_participation = proj_participation
+        developer.update()
+
+    except Exception as e:
+        print(e)
+        print(422)
+
+    return jsonify({
+            "success": True,
+            "name": name,
+            "proj_participation": proj_participation
+                    }), 200
+
+  @app.route('/developers/<int:developer_id>', methods=['DELETE'])
+  def delete_developer(developer_id):
+    error = False
+    try:
+      developer = Developer.query.get(developer_id)
+      developer.delete()
+    except Exception as e:
+      error = True
+      print(e)
+    if error:
+      abort(404)
+    else:
+      return jsonify({
+        'success': True,
+        'deleted': developer_id
+        }), 200
+    return None
+
+
+
+
 
   return app
 
--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, CheckConstraint
 from flask_sqlalchemy import SQLAlchemy
 import json
 from flask_migrate import Migrate
 
-database_path = "postgresql://mbiwdaadknzeib:b6049ba04911e612b4f9a5555e21b7468f2033b1e554c729bf2599f606827649@ec2-18-206-20-102.compute-1.amazonaws.com:5432/d84bo7akva22br"
+# works with heroku online db:  database_path = "postgresql://mbiwdaadknzeib:b6049ba04911e612b4f9a5555e21b7468f2033b1e554c729bf2599f606827649@ec2-18-206-20-102.compute-1.amazonaws.com:5432/d84bo7akva22br"
+
+#database for local development:
+
+database_name = "velocity"
+database_path = "postgresql://postgres:postgres@localhost:5432/{}".format(database_name)
 
 db = SQLAlchemy()
 
@@ -15,3 +20,54 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+
+class Developer(db.Model):
+    __tablename__ = 'developers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    proj_participation = db.Column(db.Float, db.CheckConstraint('proj_participation <= 1')) #, proj_participation >= 0
+    Vacation = db.relationship('Vacation', backref='parent', lazy=True)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Sprint(db.Model):
+    __tablename__ = 'sprints'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    sp_planned = db.Column(db.Integer)
+    sp_finished = db.Column(db.Integer)
+    velocity_factor = db.Column(db.Float)
+    sprint_fte_sum = db.Column(db.Float)
+    date_start = db.Column(db.DateTime)
+    date_end = db.Column(db.DateTime)
+    sp_predicitions_next_sprint = db.Column(db.Float)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Vacation(db.Model):
+    __tablename__ = 'vacations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    developer_id = db.Column(db.Integer, db.ForeignKey('developers.id'), nullable=False)
+    date_start = db.Column(db.DateTime, nullable=False)
+    date_end = db.Column(db.DateTime, nullable=False)
